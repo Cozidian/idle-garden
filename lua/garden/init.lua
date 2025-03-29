3afe35bdbc9ca3755de8236666c7e6861ec73709
Christoffer Berglund <christoffer.berglund@icloud.com>
2025-03-29 17:38:47 +0100

    Move init.lua

--- lua/garden/init.lua
+++ lua/garden/init.lua
@@ -1,8 +1,5 @@
 local M = {}
 
--- Add the lua directory to package path
-package.path = package.path .. ";./lua/?.lua"
-
 M.game = require("garden.game")
 M.animations = require("garden.animations")
 M.commands = require("garden.commands")
